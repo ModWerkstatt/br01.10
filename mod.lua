b1b2a5f80560ead47728bfe993c11374bc390a6c
Bandion-MW <norman.kroppen@googlemail.com>
2025-09-07 21:37:51 +0200

    initialisierung

---
+++ mod.lua
@@ -0,0 +1,25 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "europe", "locomotive", "schnellzug", "dampf", "steam", "deutsche bundesbahn",},
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+        params = {
+        },
+	},
+	options = {
+	},
+}
+end
---
+++ strings.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "BR 01.10",
+		["mod_desc"] = "BR 01.10",
+
+		["br01_10_name"] = "BR 01.10",
+		["br01_10_desc"] = "BR 01.10",
+
+	},
+	de = {
+
+		["mod_name"] = "BR 01.10",
+		["mod_desc"] = "BR 01.10",
+
+		["br01_10_name"] = "BR 01.10",
+		["br01_10_desc"] = "BR 01.10",
+
+	},
+}
+end
