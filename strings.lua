f76fa77b2f815351076b6b333698af96d1cb8a29
Dylan7654 <dylan.kreuzinger@gmail.com>
2025-09-08 22:44:06 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -14,6 +14,14 @@ return {
 		["mod_name"] = "BR 01.10",
 		["mod_desc"] = "BR 01.10",
 
+
+		["br01_10_menu"] = "DB BR 01.10",
+
+		--- BR 01 120km/h, T32 Tender---
+		["br01_10_t32_db_name"] = "BR 01.10 DB (1947)",
+		["br01_10_t32_db_desc"] = "xxxxx,,
+
+
 		["br01_10_name"] = "BR 01.10",
 		["br01_10_desc"] = "BR 01.10",
 
