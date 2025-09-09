f8fc4317b8db676ef243f313e10ba0060cdb26b7
Bandion-MW <norman.kroppen@googlemail.com>
2025-09-09 21:54:52 +0200

    Danke für den Fuck fehler fix

--- strings.lua
+++ strings.lua
@@ -19,7 +19,7 @@ return {
 
 		--- BR 01 120km/h, T32 Tender---
 		["br01_10_t32_db_name"] = "BR 01.10 DB (1947)",
-		["br01_10_t32_db_desc"] = "xxxxx,,
+		["br01_10_t32_db_desc"] = "xxxxx",
 
 
 		["br01_10_name"] = "BR 01.10",
